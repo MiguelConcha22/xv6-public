74d797d1c61b339831d7ae621d0d018e3d094230
MiguelConcha22 <mangelconcha@hotmail.com>
2019-06-28 17:33:45 -0400

    direccion de variable

--- traducirDireccion.c
+++ traducirDireccion.c
@@ -7,7 +7,8 @@ int main(void){
   int variable1 = 0;
   int* fisica = (int*)getPhysical(&variable1);  //pasa la direccion virtual y recibe la fisica
   if(fisica != 0){
-    printf(1,"Direccion fisica del proceso actual: %p", fisica);
+    printf(1,"Direccion virtual de la variable: %p", &variable1);
+    printf(1,"Direccion fisica de la variable: %p", fisica);
     exit();
   }else{
     printf(1,"Direccion entregada no es válida");
