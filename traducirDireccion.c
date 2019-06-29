97c64a4a74faba06b935ddaa0b499c7903c43ea8
MiguelConcha22 <mangelconcha@hotmail.com>
2019-06-28 23:06:46 -0400

    test2 cprintf

--- traducirDireccion.c
+++ traducirDireccion.c
@@ -5,9 +5,10 @@
 
 int main(void){
   int variable1 = 0;
+  int* virtual = &variable1;
   int* fisica = (int*)getPhysical(&variable1);  //pasa la direccion virtual y recibe la fisica
   if(fisica != 0){
-    printf(1,"Direccion virtual de la variable: %p\n", &variable1);
+    printf(1,"Direccion virtual de la variable: %p\n", virtual);
     printf(1,"Direccion fisica de la variable: %p", fisica);
     exit();
   }else{
